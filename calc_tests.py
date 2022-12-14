5d19c0393fd4b30171d44973bcbc6549be16ea4e
DmitriiPl <113013609+Pusyyyk@users.noreply.github.com>
2022-12-14 10:40:12 +0300

    Update calc_tests.py

--- calc_tests.py
+++ calc_tests.py
@@ -1,7 +1,7 @@
 import calc
 
 assert calc.calc("3 + 5") == 8, "failed test: answer should be 8"
-assert calc.calc("0 + 5") == 5, "failed test: answer should be 5"
+assert calc.calc("0 + 5") == 6, "failed test: answer should be 5"
 assert calc.calc("4 * 3") == 12, "failed test: answer should be 12"
 assert calc.calc("80 * 2") == 160, "failed test: answer should be 160"
 assert calc.calc("3 - 5") == -2, "failed test: answer should be -2"
